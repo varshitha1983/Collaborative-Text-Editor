10bea66b5dfcdc48cbcf745ec8ae4fb20b691a56
varshitha1983 <varshithamasaram@gmail.com>
2025-06-21 14:53:23 +0530

    Implement notifications feature with inbox

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,25 @@ import { mutation, query } from "./_generated/server";
 import { paginationOptsValidator } from "convex/server";
 
 
+export const getByIds = query({
+    args: { ids: v.array(v.id("documents"))},
+    handler: async (ctx, { ids }) => {
+        const documents = [];
+
+        for(const id of ids){
+            const document = await ctx.db.get(id);
+
+            if(document){
+                documents.push({id: document._id, name: document.title});
+            } else{
+                documents.push({id, name: "[Removed]"})
+            }
+        }
+
+        return documents;
+    },
+});
+
 export const create = mutation({
     args: { title: v.optional(v.string()), initialContent: v.optional(v.string())},
     handler: async (ctx, args) => {
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -22,6 +22,7 @@ import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 export const Navbar = () => {
     const { editor } = useEditorStore();
@@ -215,6 +216,7 @@ export const Navbar = () => {
             </div>
             <div className="flex gap-3 items-center pl-6">
                 <Avatars />
+                <Inbox />
                 <OrganizationSwitcher 
                     afterCreateOrganizationUrl="/"
                     afterLeaveOrganizationUrl="/"
