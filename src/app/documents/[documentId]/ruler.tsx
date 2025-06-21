6187c496aa8a323c467fe99747865838ebcfe4dc
varshitha1983 <varshithamasaram@gmail.com>
2025-06-21 15:09:28 +0530

    Collaborative margin ruler

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,13 +1,24 @@
 import { useRef, useState } from "react";
 import {FaCaretDown} from "react-icons/fa";
+import { useMutation, useStorage } from "@liveblocks/react";
 
 const markers = Array.from({ length: 83}, (_, i) => i);
 
 
 export const Ruler = () => {
-    const [leftMargin, setLeftMargin] = useState(56);
-    const [rightMargin, setRightMargin] = useState(56);
 
+    const leftMargin = useStorage((root) => root.leftMargin) ?? 56;
+    const rightMargin = useStorage((root) => root.rightMargin) ?? 56;
+
+    const setLeftMargin = useMutation(({storage}, position: number) => {
+        storage.set("leftMargin", position);
+    },[]);
+
+    const setRightMargin = useMutation(({storage}, position: number) => {
+        storage.set("rightMargin", position);
+    },[]);
+
+    
     const[isDraggingLeft, setIsDraggingLeft] = useState(false);
     const [isDraggingRight, setIsDraggingRight] = useState(false);
 
@@ -34,7 +45,7 @@ export const Ruler = () => {
                 if(isDraggingLeft){
                     const maxLeftPosition = PAGE_WIDTH - rightMargin - MIN_WIDTH;
                     const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
-                    setLeftMargin(newLeftPosition);//TODO: Make Collaborative
+                    setLeftMargin(newLeftPosition);
                 } else if(isDraggingRight){
                     const maxRightPosition = PAGE_WIDTH - (leftMargin + MIN_WIDTH);
                     const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
